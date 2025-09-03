9d4a6ff9e86b09638f68b7a1319f0d042c210695
Bekzat Raiimkulov <raiimkulov.bekzat@gmail.com>
2025-09-03 22:22:02 +0600

    lombok init

--- src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.catsgram.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 // base url for the whole controller
 @RequestMapping("/api/v1/home")
 public class SimpleController {
+    // creating a simple logger
+    private static final Logger log = LoggerFactory.getLogger(SimpleController.class);
+
     // tells spring that it is a resource URL
     @GetMapping("/hello")
     public String homePage() {
+        // logging the get request was done
+        log.info("/hello request was called");
         return "Котограм";
     }
 }
