d3d4798308607a59adb7ec7c5dc690edb3add285
Bekzat Raiimkulov <raiimkulov.bekzat@gmail.com>
2025-09-03 22:48:37 +0600

    check

--- src/main/java/ru/yandex/practicum/catsgram/model/Post.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/Post.java
@@ -1,40 +1,18 @@
 package ru.yandex.practicum.catsgram.model;
 
+import lombok.*;
+
 import java.time.Instant;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Post {
+    @NonNull
     private final String author;
     private final Instant creationDate = Instant.now();
     private String description;
     private String photoUrl;
-
-    public Post(String author, String description, String photoUrl) {
-        this.author = author;
-        this.description = description;
-        this.photoUrl = photoUrl;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public Instant getCreationDate() {
-        return creationDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getPhotoUrl() {
-        return photoUrl;
-    }
-
-    public void setPhotoUrl(String photoUrl) {
-        this.photoUrl = photoUrl;
-    }
 }
