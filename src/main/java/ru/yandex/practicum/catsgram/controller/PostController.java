f1b18b846fa3eceb90fd3af02f1938150f4d9e0c
Bekzat Raiimkulov <raiimkulov.bekzat@gmail.com>
2025-09-12 08:35:04 +0600

    request param, request body

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -1,15 +1,13 @@
 package ru.yandex.practicum.catsgram.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.service.PostService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PostController {
@@ -29,5 +27,18 @@ public class PostController {
     @PostMapping("/posts")
     public Post create(@RequestBody Post post) {
         return postService.create(post);
+    }
+
+    // REST rules say that id should be in the URL - it is called path variable
+    @GetMapping("/posts/{postId}")
+    public Optional<Post> findById(@PathVariable int postId) {
+        return postService.findById(postId);
+    }
+
+    // /posts?user=user123 is a request parameter in a url
+    @GetMapping("/posts/search")
+    public List<Post> searchPosts(@RequestParam String author) {
+        System.out.println("searching for the name " + author);
+        return null;
     }
 }
--- src/main/java/ru/yandex/practicum/catsgram/model/Post.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/Post.java
@@ -10,6 +10,7 @@ import java.time.Instant;
 @ToString
 @EqualsAndHashCode
 public class Post {
+    private Integer id; // to get a specific post by id
     @NonNull
     private final String author;
     private final Instant creationDate = Instant.now();
